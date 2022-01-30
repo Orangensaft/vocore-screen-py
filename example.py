efddb57ef49db59c104fb42ad8bc29087d5da448
Nicolas Meisberger <nico@bitshift.wtf>
2022-01-30 17:08:37 +0100

    Updated example, removed unused files

--- example.py
+++ example.py
@@ -1,13 +1,12 @@
-import random
-from pathlib import Path
+from font import FONT_5x7
 
 from screen import VocoreScreen
 
 if __name__ == "__main__":
-    new_brightness = random.randint(0, 255)
-    picture = Path("example_frames/frame.dat").read_bytes()
-    print(f"Setting brightness to {new_brightness}")
-
     d = VocoreScreen()
-    d.set_brightness(new_brightness)
-    d._set_frame(picture)
+    d.set_brightness(255)
+    d.draw_string(45, 430, "".join([str(c) for c in FONT_5x7 if c != -1]), "#00FF00")
+    d.draw_rect(50,50,100,100,"#FF24FF")
+    d.draw_rect(160,50,260,150,"#00FFFF")
+    d.draw_string(300, 150, "Hello, World!", "#FFBB00")
+    d.blit()  # Update screen
