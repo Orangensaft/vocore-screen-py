9ef24391851898d58896e3ad97b882bca5befdbf
Nicolas Meisberger <nico@bitshift.wtf>
2022-01-30 17:08:56 +0100

    Formatting

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     d = VocoreScreen()
     d.set_brightness(255)
     d.draw_string(45, 430, "".join([str(c) for c in FONT_5x7 if c != -1]), "#00FF00")
-    d.draw_rect(50,50,100,100,"#FF24FF")
-    d.draw_rect(160,50,260,150,"#00FFFF")
+    d.draw_rect(50, 50, 100, 100, "#FF24FF")
+    d.draw_rect(160, 50, 260, 150, "#00FFFF")
     d.draw_string(300, 150, "Hello, World!", "#FFBB00")
     d.blit()  # Update screen
